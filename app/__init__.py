a6c857643958994ad43d36e78d169cd8a2575ef8
Craig Ballingall <craig.ballingall@gmail.com>
2018-05-24 10:03:39 +0000

    merge changes

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,7 @@ from flask import Flask
 from config import config
 
 app = Flask(__name__)
+SECRET_KEY=config.Config.SECRET_KEY
+print(SECRET_KEY)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 from config import config
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -19,4 +20,10 @@ def index():
                 ]
 
         return render_template('index.html', title='Home', user=user, posts=posts, secret_key = runningConfig.SECRET_KEY)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html',title='Sign In', form=form)
 
